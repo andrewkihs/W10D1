2f244f5f688a807797cdb0cfa913889865576ccb
Robby Balistreri <rbalistreri5@gmail.com>
2021-04-26 13:16:35 -0500

    Fill out initial DOMCollection methods

--- src/dom_node_collection.js
+++ src/dom_node_collection.js
@@ -35,6 +35,43 @@ class DOMNodeCollection {
             });
         })
     }
+
+    attr(attributeName, value) {
+        if (typeof value == 'undefined') {
+            //getter
+            let result = this.arr[0].getAttribute(attributeName);
+            return result;
+        } else {
+            this.arr.forEach ( (ele) => {
+                ele.setAttribute(attributeName, value);
+            })
+        }
+    }
+
+    addClass(className) {
+        this.arr.forEach((ele) => {
+            let currClasses = ele.getAttribute('class') || '';
+            currClasses += ` ${className}`;
+            ele.setAttribute('class', currClasses.trim());
+        })
+    }
+
+    removeClass(className) {
+        this.arr.forEach((ele) => {
+            let currClasses = ele.getAttribute('class');
+            let currClassesArr = currClasses.split(' ');
+            console.log(`currClassesArr = ${currClassesArr}`)
+            let newArr = [];
+            newArr = currClassesArr.filter(ele => ele !== className);
+            console.log(`newArr = ${newArr}`)
+            let newClasses = newArr.join(' ')
+            if (newClasses == '') {
+                ele.removeAttribute('class');
+            } else {
+            ele.setAttribute('class', newClasses.trim());
+            }
+        })
+    }
 }
 
 module.exports = DOMNodeCollection
